ba48c3d9a988e06f1c50affd3fb0954e8428bddf
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-01-05 22:23:29 +0000

    Some katas

---
+++ Katas/StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (IsNumbersEmpty(numbers))
+            {
+                return 0;
+            }
+
+            if (IsSingleNumber(numbers))
+            {
+                return int.Parse(numbers);
+            }
+
+            var splitNumbers = numbers.Split(',');
+
+            return splitNumbers.Sum(sn => int.Parse(sn));
+        }
+
+        private static bool IsSingleNumber(string numbers)
+        {
+            return numbers.Length == 1;
+        }
+
+        private static bool IsNumbersEmpty(string numbers)
+        {
+            return numbers == string.Empty;
+        }
+    }
+}
