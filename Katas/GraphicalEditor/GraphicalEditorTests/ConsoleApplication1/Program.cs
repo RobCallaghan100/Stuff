c820a834612322b4d378780fa7743074762b0ffd
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-01-11 21:43:10 +0000

    Added console project to run the code for real

---
+++ Katas/GraphicalEditor/GraphicalEditorTests/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GraphicalEditor;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = new Command();
+            string line = "";
+
+            do
+            {
+                line = Console.ReadLine();
+                command.Input(line);
+
+            } while (line.ToUpper() != "X");
+
+            Console.ReadLine();
+        }
+    }
+}
