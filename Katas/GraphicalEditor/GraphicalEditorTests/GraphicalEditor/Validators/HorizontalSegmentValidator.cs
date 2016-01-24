0cd7c59c1d4ed5f9516bffdd17ff28469ffb7162
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-01-24 12:36:23 +0000

    Adding horzontalsegment validation tests

---
+++ Katas/GraphicalEditor/GraphicalEditorTests/GraphicalEditor/Validators/HorizontalSegmentValidator.cs
@@ -0,0 +1,85 @@
+﻿using GraphicalEditor.Interfaces;
+
+namespace GraphicalEditor.Validators
+{
+    using System;
+
+    public class HorizontalSegmentValidator : IValidator
+    {
+        public bool IsValid(string[] args)
+        {
+            if (!IsExactlyFiveArguments(args))
+            {
+                return false;
+            }
+
+            if (!IsFirstArgumentAn_H(args))
+            {
+                return false;
+            }
+
+//            if (!IsArgumentAnInt(args[1]))
+//            {
+//                return false;
+//            }
+//
+//           
+//
+//            if (!IsArgumentAnInt(args[2]))
+//            {
+//                return false;
+//            }
+//
+//            if (!IsArgumentAnInt(args[3]))
+//            {
+//                return false;
+//            }
+//
+//            if (!IsArgumentInRange(args[1]))
+//            {
+//                return false;
+//            }
+//
+//            if (!IsArgumentInRange(args[2]))
+//            {
+//                return false;
+//            }
+//
+//            if (!IsArgumentInRange(args[3]))
+//            {
+//                return false;
+//            }
+
+            return true;
+        }
+
+        private static bool IsArgumentInRange(string arg)
+        {
+            int value = 0;
+            Int32.TryParse(arg, out value);
+
+            return value >= 1 && value <= 250;
+        }
+
+        private static bool IsArgumentAnInt(string arg)
+        {
+            int value = 0;
+            if (!int.TryParse(arg, out value))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsFirstArgumentAn_H(string[] args)
+        {
+            return args[0].ToUpper().Trim() == "H";
+        }
+
+        private static bool IsExactlyFiveArguments(string[] args)
+        {
+            return args.Length == 5;
+        }
+    }
+}
---
+++ Katas/GraphicalEditor/GraphicalEditorTests/GraphicalEditorTests/Validators/HorizontalSegmentValidatorTests.cs
@@ -0,0 +1,104 @@
+﻿using GraphicalEditor.Validators;
+using NUnit.Framework;
+
+namespace GraphicalEditorTests.Validators
+{
+    [TestFixture]
+    public class HorizontalSegmentValidatorTests
+    {
+        private HorizontalSegmentValidator _horizontalSegmentValidator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _horizontalSegmentValidator = new HorizontalSegmentValidator();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _horizontalSegmentValidator = null;
+        }
+
+
+        [TestCase(false, new string[] { "H" })]
+        [TestCase(false, new string[] { "H", "1" })]
+        [TestCase(false, new string[] { "H", "1", "2" })]
+        [TestCase(false, new string[] { "H", "1", "2", "3" })]
+        [TestCase(false, new string[] { "H", "1", "2", "3", "A", "A" })]
+        public void ShouldReturnFalseIfThereAreNotExactlyFiveArguments(bool expectedResult, string[] args)
+        {
+            var result = _horizontalSegmentValidator.IsValid(args);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ShouldReturnFalseIfIfFirstParameterIsNotH()
+        {
+            var result = _horizontalSegmentValidator.IsValid(new string[] { "X", "1", "2", "3", "A" });
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+//
+//        [Test]
+//        public void ShouldReturnFalseIfSecondParameterIsNotAnInt()
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new string[] { "V", "X", "2", "3", "A" });
+//
+//            Assert.That(result, Is.EqualTo(false));
+//        }
+//
+//        [Test]
+//        public void ShouldReturnFalseIfThirdParameterIsNotAnInt()
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new string[] { "V", "1", "X", "3", "A" });
+//
+//            Assert.That(result, Is.EqualTo(false));
+//        }
+//
+//        [Test]
+//        public void ShouldReturnFalseIfFourthParameterIsNotAnInt()
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new string[] { "V", "1", "2", "X", "A" });
+//
+//            Assert.That(result, Is.EqualTo(false));
+//        }
+//
+//        [Test]
+//        public void ShouldReturnTrueIfPassedExpectedFormat()
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new string[] { "V", "1", "2", "3", "A" });
+//
+//            Assert.That(result, Is.EqualTo(true));
+//        }
+//
+//        [TestCase("0")]
+//        [TestCase("251")]
+//        public void ShouldReturnFalseIfSecondArgumentIsNotBetween1And250(string arg)
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new[] { "V", arg, "2", "3", "A" });
+//
+//            Assert.That(result, Is.False);
+//        }
+//
+//        [TestCase("0")]
+//        [TestCase("251")]
+//        public void ShouldReturnFalseIfThirdArgumentIsNotBetween1And250(string arg)
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new[] { "V", "1", arg, "3", "A" });
+//
+//            Assert.That(result, Is.False);
+//        }
+//
+//        [TestCase("0")]
+//        [TestCase("251")]
+//        public void ShouldReturnFalseIfFourthArgumentIsNotBetween1And250(string arg)
+//        {
+//            var result = _verticalSegmentValidator.IsValid(new[] { "V", "1", "2", arg, "A" });
+//
+//            Assert.That(result, Is.False);
+//        }
+
+    }
+}
