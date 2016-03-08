d8b13fc640ef3a3e23d2b17ea59242f3421ba243
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-08 21:57:54 +0000

    Adding constraints to both versions of the controllers

--- C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -1,10 +1,19 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
 using WebApi2Book.Common.Routing;
+using WebApi2Book.Web.Api.Models;
 
 namespace WebApi2Book.Web.Api.Controllers.V1
 {
     [ApiVersion1RoutePrefix("tasks")]
     public class TasksController : ApiController
     {
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {
+            return new Task
+            {
+                Subject = "In v1, newTask.subject = " + newTask.Subject
+            };
+        }
     }
 }
