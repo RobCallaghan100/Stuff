c953013be3428a01e6cde409e82f4130640c60ae
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-09 10:42:16 +0000

    Added ninject configuration

---
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApi2Book.Web.Api
+{
+    using log4net.Config;
+    using Ninject;
+    using WebApi2Book.Common;
+    using WebApi2Book.Common.Logging;
+
+    public class NinjectConfigurator
+    {
+        public void Configure(IKernel container)
+        {
+            AddBindings(container);
+        }
+
+        private void AddBindings(IKernel container)
+        {
+            Configurelog4net(container);
+
+            container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
+        }
+
+        private void Configurelog4net(IKernel container)
+        {
+            XmlConfigurator.Configure();
+
+            var logManager = new LogManagerAdapter();
+            container.Bind<ILogManager>().ToConstant(logManager);
+        }
+    }
+}
