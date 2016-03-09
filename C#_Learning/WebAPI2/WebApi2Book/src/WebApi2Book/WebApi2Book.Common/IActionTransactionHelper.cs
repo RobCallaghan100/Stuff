b0a9278c22c0c962eec59bfb7c8ad62d56120ad6
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-09 17:07:38 +0000

    Filters

---
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Common/IActionTransactionHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi2Book.Common
+{
+    using System.Web.Http.Filters;
+
+    public interface IActionTransactionHelper
+    {
+        void BeginTransaction();
+        void EndTransaction(HttpActionExecutedContext filterContext);
+        void CloseSession();
+    }
+}
