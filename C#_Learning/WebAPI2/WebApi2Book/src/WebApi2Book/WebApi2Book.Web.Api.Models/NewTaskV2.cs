ad1d67b0c714e792217049c430514cf55717e6bb
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-11 20:22:45 +0000

    Add NewTask

---
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api.Models/NewTaskV2.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi2Book.Web.Api.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class NewTaskV2
+    {
+        public string Subject { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public User Assignee { get; set; }
+    }
+}
+    
