8447f7427b3e57136b5c369f9e8d1a7d748c72fc
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-12 10:36:17 +0000

    More Automapper

---
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api/AutoMappingConfiguration/TaskAssigneesResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApi2Book.Web.Api.AutoMappingConfiguration
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using Data.Entities;
+    using WebApi2Book.Common;
+    using WebApi2Book.Common.TypeMapping;
+
+    public class TaskAssigneesResolver : ValueResolver<Task, List<User>>
+    {
+        public IAutoMapper AutoMapper
+        {
+            get { return WebContainerManager.Get<IAutoMapper>(); }
+        }
+
+        protected override List<User> ResolveCore(Task source)
+        {
+            return source.Users.Select(x => AutoMapper.Map<User>(x)).ToList();
+        }
+    }
+}
