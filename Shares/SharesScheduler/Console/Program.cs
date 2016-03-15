d6bdf6a0b912c2c8e8a9999c5a4c26e6edbd662b
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-15 15:44:10 +0000

    Adding outline of integration tests

--- Shares/SharesScheduler/Console/Program.cs
+++ Shares/SharesScheduler/Console/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Console
+﻿namespace Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello Shares!");
         }
     }
 }
---
+++ Shares/SharesScheduler/SharesSchedulerTests/Integration/IntegrationTests.cs
@@ -0,0 +1,18 @@
+﻿namespace SharesSchedulerTests.Integration
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IntegrationTests
+    {   
+        [Test]
+        public void ShouldGetDataFromElasticSearchAfterPopulatingFromCallToYahooFinance()
+        {
+            /*
+            go to yahoo finance
+            get data for vod
+            populate elastic search
+            */
+        }
+    }
+}
