54106092b3cc88cd73d7a146d665b2656db680db
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-16 22:21:25 +0000

    Connects to Yahoo finance and returns a Price object

--- Shares/SharesScheduler/SchedulerServices/YahooFinanceClient.cs
+++ Shares/SharesScheduler/SchedulerServices/YahooFinanceClient.cs
@@ -1,9 +1,13 @@
-﻿namespace SchedulerServices
+﻿using System.Diagnostics;
+using System.IO;
+using Models;
+using static System.Decimal;
+
+namespace SchedulerServices
 {
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
-    using Models;
 
     public class YahooFinanceClient : IFinanceClient, IDisposable
     {
@@ -20,20 +24,54 @@
         public async Task<Price> Get(string epicCode, DateTime dateTime)
         {
             var price = new Price();
-            var queryString = $"table.csv?s={epicCode}&a{dateTime.Month - 1}&b={dateTime.Day}&c={dateTime.Year}&d={dateTime.Month-1}&e={dateTime.Day}&f={dateTime.Year}&g=d";
-            using (var response = await _httpClient.GetAsync(queryString))
-            using (var content = response.Content)
+
+            try
             {
-                TODO: change stream and get back as csv
+                var queryString = $"table.csv?s={epicCode}&a={dateTime.Month - 1}&b={dateTime.Day}&c={dateTime.Year}&d={dateTime.Month - 1}&e={dateTime.Day}&f={dateTime.Year}&g=d";
+                using (var response = await _httpClient.GetAsync(queryString))
+                using (var content = response.Content)
+                {
+                    var stream = await content.ReadAsStreamAsync();
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        int lineNumber = 0;
+                        string line = "";
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            // TODO: validate that column headers are correct
+
+                            if (lineNumber > 0)
+                            {
+                                var splitLine = line.Split(',');
+                                var splitEpicCode = epicCode.Split('.');
+
+                                price.Epic = epicCode;
+                                price.Date = DateTime.Parse(splitLine[0]);
+                                price.Open = Parse(splitLine[1]);
+                                price.High = Parse(splitLine[2]);
+                                price.Low = Parse(splitLine[3]);
+                                price.Close = Parse(splitLine[4]);
+                                price.Volume = Parse(splitLine[5]);
+                                price.AdjustedClose = Parse(splitLine[6]);
+
+                                if (splitEpicCode.Length >= 1)
+                                {
+                                    price.Market = splitEpicCode[1];
+                                }
+                            }
+
+                            lineNumber++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: logging  
+                throw;
             }
 
             return price;
-
-            // &ignore=.csv
-            // http://real-chart.finance.yahoo.com/table.csv?s=VOD.L&a=02&b=15&c=2016&d=02&e=15&f=2016&g=d&ignore=.csv
-            //             https://uk.finance.yahoo.com/q/hp?s=VOD.L&b=14&a=01&c=2016&e=15&d=02&f=2016&g=d
-            // https://uk.finance.yahoo.com/q/hp?s=VOD.L  &a=00 &b=01 &c=2016 | &d=11 &e=31 &f=2016 &g=d
-            //_httpClient.BaseAddress
         }
 
         public void Dispose()
--- Shares/SharesScheduler/SchedulerServicesTests/YahooFinanceClientTests.cs
+++ Shares/SharesScheduler/SchedulerServicesTests/YahooFinanceClientTests.cs
@@ -11,20 +11,21 @@
         [Test]
         public async Task ShouldReturnPriceFromGet()
         {
-            string epicCode = "VOD";
+            string epicCode = "VOD.L";
             var yahooFinanceClient = new YahooFinanceClient();
 
             var dateTime = new DateTime(2016, 1, 4);
             var price = await yahooFinanceClient.Get(epicCode, dateTime);
 
             Assert.That(price, Is.Not.Null);
-            Assert.That(price.Epic, Is.EqualTo("VOD"));
+            Assert.That(price.Epic, Is.EqualTo("VOD.L"));
+            Assert.That(price.Date, Is.EqualTo(new DateTime(2016, 1, 4)));
             Assert.That(price.Open, Is.GreaterThan(0M));
             Assert.That(price.High, Is.GreaterThan(0M));
             Assert.That(price.Low, Is.GreaterThan(0M));
             Assert.That(price.Close, Is.GreaterThan(0M));
             Assert.That(price.Volume, Is.GreaterThan(0M));
-            Assert.That(price.Market, Is.GreaterThan(0M));
+            Assert.That(price.Market, Is.EqualTo("L"));
         }
 
         // TODO: get back range of dates
