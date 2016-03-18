a8cd46c67c413d9e94f4eb05dcc720db6ad2f236
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-18 14:01:46 +0000

    Tip calculator

--- C#_Learning/ExercisesForProgrammers57/Prologue/TipCalculator/TipCalculator/Program.cs
+++ C#_Learning/ExercisesForProgrammers57/Prologue/TipCalculator/TipCalculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TipCalculatorConsole
 {
@@ -10,7 +6,28 @@ namespace TipCalculatorConsole
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter bill");
+                var billString = Console.ReadLine();
+                var bill = decimal.Parse(billString);
+
+                Console.WriteLine("Enter tip");
+                var tipString = Console.ReadLine();
+                var tip = decimal.Parse(tipString);
+
+                var tipCalculator = new TipCalculator.TipCalculator();
+
+                var result = tipCalculator.Calculate(bill, tip);
+
+                Console.WriteLine($"Bill amount: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem! {0}", ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
-    
