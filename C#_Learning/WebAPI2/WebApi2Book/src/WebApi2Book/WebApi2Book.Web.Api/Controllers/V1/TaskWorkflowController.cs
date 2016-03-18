3843cbd440c4572f0ce6d429411eb3ff26049554
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-18 11:19:18 +0000

    Created a controller for task workflow

---
+++ C#_Learning/WebAPI2/WebApi2Book/src/WebApi2Book/WebApi2Book.Web.Api/Controllers/V1/TaskWorkflowController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using WebApi2Book.Common;
+using WebApi2Book.Common.Routing;
+using WebApi2Book.Data.QueryProcessors;
+using WebApi2Book.Web.Api.Models;
+using WebApi2Book.Web.Common;
+
+namespace WebApi2Book.Web.Api.Controllers.V1
+{
+    [ApiVersion1RoutePrefix("")]
+    [UnitOfWorkActionFilter]
+    public class TaskWorkflowController : ApiController
+    {
+        private readonly IStartTaskWorkflowProcessor _startTaskWorkflowProcessor;
+
+        public TaskWorkflowController(IStartTaskWorkflowProcessor startTaskWorkflowProcessor)
+        {
+            _startTaskWorkflowProcessor = startTaskWorkflowProcessor;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Constants.RoleNames.SeniorWorker)]
+        [Route("tasks/{taskId:long}/activations", Name = "StartTaskRoute")]
+        public Task StartTask(long taskId)
+        {
+            var task = _startTaskWorkflowProcessor.StartTask(taskId);
+
+            return task;
+        }
+    }
+}
