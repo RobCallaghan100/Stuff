0564e870959b962c1e84f48985f5847989a11889
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-18 12:55:39 +0000

    Practice programs

---
+++ C#_Learning/ExercisesForProgrammers57/Prologue/TipCalculator/TipCalculatorTests/TipCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TipCalculator
+{
+    public class TipCalculator
+    {
+        public decimal Calculate(decimal billAmount, decimal tip)
+        {
+            return billAmount + Math.Round((tip/100)*billAmount, 2);
+        }
+    }
+}
---
+++ C#_Learning/ExercisesForProgrammers57/Prologue/TipCalculator/TipCalculatorTests/TipCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TipCalculator
+{
+    [TestFixture]
+    public class TipCalculatorTests
+    {
+        [TestCase(100, 15, 115)]
+        [TestCase(110, 7.5, 118.25)]
+        [TestCase(11.25, 15, 12.94)]
+        [TestCase(9.81, 0, 9.81)]
+        public void ShouldReturn115IfBillAmount100AndTip15(decimal billAmount, decimal tip, decimal expectedTotalAmount)
+        {
+            var tipCalculator = new TipCalculator();
+
+            var result = tipCalculator.Calculate(billAmount, tip);
+
+            Assert.That(result, Is.EqualTo(expectedTotalAmount));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfTipBelowZero(decimal billAmount, decimal tip, decimal expectedTotalAmount)
+        {
+            var tipCalculator = new TipCalculator();
+
+            var result = tipCalculator.Calculate(billAmount, tip);
+
+            Assert.Throws(typeof(Exception), ))
+        }
+    }
+}
