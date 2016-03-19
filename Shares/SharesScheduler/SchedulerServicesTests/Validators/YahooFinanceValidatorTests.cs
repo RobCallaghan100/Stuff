3ddc6c9476f06f8e62871d3418dff46277d9bb14
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-03-19 14:22:47 +0000

    Tests for wrong header values

--- Shares/SharesScheduler/SchedulerServicesTests/Validators/YahooFinanceValidatorTests.cs
+++ Shares/SharesScheduler/SchedulerServicesTests/Validators/YahooFinanceValidatorTests.cs
@@ -17,5 +17,22 @@ namespace SchedulerServicesTests.Validators
             Assert.That(result.IsValid, Is.True);
             Assert.That(string.IsNullOrEmpty(result.Message), Is.True);
         }
+
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "DateX", "Open", "High", "Low", "Close", "Volume", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "OpenX", "High", "Low", "Close", "Volume", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "Open", "HighX", "Low", "Close", "Volume", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "Open", "High", "LowX", "Close", "Volume", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "Open", "High", "Low", "CloseX", "Volume", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "Open", "High", "Low", "Close", "VolumeX", "Adj Close" })]
+        [TestCase(false, "Expecting columns in the following order: Date,Open,High,Low,Close,Volume,Adj Close", new[] { "Date", "Open", "High", "Low", "Close", "Volume", "Adj CloseX" })]
+        public void ShouldCheckHeadersReturnValidIsFalseIfHeadersAreNotAsExpected(bool expectedValid, string expectedMessage, string[] line)
+        {
+            var yahooFinanceValidator = new YahooFinanceValidator();
+
+            var result = yahooFinanceValidator.CheckHeaders(line);
+
+            Assert.That(result.IsValid, Is.EqualTo(expectedValid));
+            Assert.That(result.Message, Is.EqualTo(expectedMessage));
+        }
     }
 }
