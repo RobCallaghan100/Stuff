6a6081564b3d54b8ccde719d9f7439a3255ee194
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-03-31 12:02:01 +0100

    MVC and tidying up

---
+++ C#_Learning/MVC/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {name}, NumTimes is: {id}");
+        }
+    }
+}
