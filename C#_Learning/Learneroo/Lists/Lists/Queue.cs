f08558f5130877d52f69014e9d7b90a7ea906ba0
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-04-09 14:56:52 +0100

    Finish queue

--- C#_Learning/Learneroo/Lists/Lists/Queue.cs
+++ C#_Learning/Learneroo/Lists/Lists/Queue.cs
@@ -2,13 +2,31 @@
 {
     public class Queue<T>
     {
+        private LinkedList<T> _linkedList;
+
+        public Queue()
+        {
+            _linkedList = new LinkedList<T>();
+        }
+
         public void Enqueue(T value)
         {
+            _linkedList.Add(value);
+        }
+
+        public T Dequeue()
+        {
+            return _linkedList.Remove(0);
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            return _linkedList.Head.Value;
+        }
+
+        public int Count()
+        {
+            return _linkedList.Size;
         }
     }
 }
--- C#_Learning/Learneroo/Lists/Lists/QueueTest.cs
+++ C#_Learning/Learneroo/Lists/Lists/QueueTest.cs
@@ -6,19 +6,31 @@ namespace Lists
     public class QueueTest
     {
         [Test]
-        public void ShouldAddToQueue()
+        public void ShouldEnQueue()
         {
-
             var queue = new Queue<int>();
 
             queue.Enqueue(1);
             queue.Enqueue(2);
 
             Assert.That(queue.Peek(), Is.EqualTo(1));
-
-            var x = new System.Collections.Generic.Queue<int>();
-            x.Enqueue(1);
-            x.Dequeue();
+            Assert.That(queue.Count(), Is.EqualTo(2));
         }
+
+        [Test]
+        public void ShouldDeQueue()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            var result = queue.Dequeue();
+
+            Assert.That(result, Is.EqualTo(1));
+            Assert.That(queue.Peek(), Is.EqualTo(2));
+            Assert.That(queue.Count(), Is.EqualTo(1));
+        }
+
+        // TODO: check we can not dequeue too many
     }
 }
