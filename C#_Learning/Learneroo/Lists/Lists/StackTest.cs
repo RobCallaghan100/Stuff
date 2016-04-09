e5c192c22ac508b6a94862f95527d56bce6fa72d
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-04-09 13:50:00 +0100

    Added acceptance test

--- C#_Learning/Learneroo/Lists/Lists/StackTest.cs
+++ C#_Learning/Learneroo/Lists/Lists/StackTest.cs
@@ -41,6 +41,20 @@ namespace Lists
             Assert.That(_stack.Peek(), Is.EqualTo(1));
         }
 
+        [Test]
+        public void ShouldPassAcceptanceTestForPushAndPop()
+        {
+            _stack.Push(3);
+            _stack.Push(5);
+            Assert.That(_stack.Pop(), Is.EqualTo(5));
+            Assert.That(_stack.Pop(), Is.EqualTo(3));
+            _stack.Push(2);
+            _stack.Push(7);
+            _stack.Push(11);
+            Assert.That(_stack.Pop(), Is.EqualTo(11));
+            Assert.That(_stack.Pop(), Is.EqualTo(7));
+        }
+
         // TODO: test to make sure an exception is raised when popping too many
     }
 }
