fb3440de04829927d6a85d743432817c591c38f7
Robert Callaghan <rob.callaghan@argusmedia.com>
2016-04-11 10:04:37 +0100

    Recursion

---
+++ C#_Learning/Learneroo/Recursion/Recursion/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Recurse(0);
+
+            //var result = Factorial(3);
+            //var result = DoStuff(4);
+
+            int[] arr = {1, 5, 8, 12, 17, 19};
+            var result = BinarySearch(arr, 16, 0, arr.Length - 1);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static int BinarySearch(int[] array, int n, int startIndex, int endIndex)
+        {
+            // {1, 5, 8, 12, 16, 19};-16
+            var midIndex = (startIndex + endIndex)/2;
+            var midValue = array[midIndex];
+
+            if (endIndex < startIndex)
+            {
+                return -1;
+            }
+
+            if (midValue == n)
+            {
+                return midIndex;
+            }
+            else if (midValue < n)
+            {
+                startIndex = midIndex;
+                return BinarySearch(array, n, startIndex-1, endIndex);
+            }
+            else
+            {
+                endIndex = midIndex;
+                return BinarySearch(array, n, startIndex+1, endIndex);
+            }
+        }
+
+        private static int DoStuff(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n + DoStuff(n - 1);
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+        private static void Recurse(int i)
+        {
+            if (i == 10)
+            {
+                return;
+            }
+
+            Console.WriteLine(i);
+            Recurse(i + 1);
+        }
+    }
+}
