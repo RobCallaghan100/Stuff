8265fbb62df73226034f431832d6c4c8440bab04
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-04-14 21:04:55 +0100

    js

--- AngularJS/Todo/todo.js
+++ AngularJS/Todo/todo.js
@@ -1,18 +1,22 @@
-(function(){
+(function () {
     
     "use strict";
     
-    var app = angular.module('todoApp', ['lodash']);
+    var app = angular.module('todoApp', [])
+        .constant('_', window._)
+        .run(function ($rootScope) {
+            $rootScope._ = window._;
+        });
     
     
-    app.controller('todoCtrl', [ '$scope', '_', function($scope, _){
+    app.controller('todoCtrl', [ '$scope', '_', function ($scope, _) {
         
         $scope.todos = [];
         
-        $scope.AddTodo = function(value){
+        $scope.AddTodo = function (value) {
             
-            if (value){
-                var newTodo = { 'description':value, 'completed':false };
+            if (value) {
+                var newTodo = { 'description': value, 'completed': false };
                 $scope.todos.push(newTodo);
                 $scope.newTodo = '';
             }
@@ -20,8 +24,14 @@
 
         //http://stackoverflow.com/questions/29768009/how-to-install-underscore-js-in-my-angular-application
         
+        $scope.removeTodo = function(todo){
+           _.remove($scope.todos, function(t){
+               return t.description == todo.description;
+           });
+        };
         
-        $scope.$watch('todos', function(){
+        
+        $scope.$watch('todos', function () {
            // alert('watch');
         });
     }]);
