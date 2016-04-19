947eeaf27c4514c10e82c1666d1a8044194964c6
RobCallaghan100 <R_Callaghan@yahoo.com>
2016-04-19 10:47:23 +0100

    Adding javascript and EF6 stuff

---
+++ EF6/Ninja.Module2Demo/CodeModelFromDB/AWModel.cs
@@ -0,0 +1,75 @@
+namespace CodeModelFromDB
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AWModel : DbContext
+    {
+        public AWModel()
+            : base("name=AWModel")
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+        public virtual DbSet<ProductDescription> ProductDescriptions { get; set; }
+        public virtual DbSet<ProductModel> ProductModels { get; set; }
+        public virtual DbSet<ProductModelProductDescriptionCulture> ProductModelProductDescriptionCultures { get; set; }
+        public virtual DbSet<vProductAndDescription> vProductAndDescriptions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(e => e.StandardCost)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ListPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.SizeUnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.WeightUnitMeasureCode)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Weight)
+                .HasPrecision(8, 2);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ProductLine)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Class)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Style)
+                .IsFixedLength();
+
+            modelBuilder.Entity<ProductDescription>()
+                .HasMany(e => e.ProductModelProductDescriptionCultures)
+                .WithRequired(e => e.ProductDescription)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductModel>()
+                .HasMany(e => e.ProductModelProductDescriptionCultures)
+                .WithRequired(e => e.ProductModel)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ProductModelProductDescriptionCulture>()
+                .Property(e => e.CultureID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<vProductAndDescription>()
+                .Property(e => e.CultureID)
+                .IsFixedLength();
+        }
+    }
+}
---
+++ Javascript/WierdParts/11Undefined/B5-Undefined/B5_undefined/Starter/app.js
@@ -0,0 +1,11 @@
+var a = 'sddsds';
+console.log(a);
+
+a;
+
+if (a === undefined){
+    console.log('a is undefined');
+}
+else{
+    console.log('a IS defined');
+}
